e55522a3d67bd423d810783334188ef957447305
Frederic Merizen <frederic.merizen@gmail.com>
2017-07-25 11:57:58 +0200

    Fully component-ise
    Avoid using global state

--- src/clj/example/server.clj
+++ src/clj/example/server.clj
@@ -4,16 +4,16 @@
 
   (:require
     [ring.middleware.defaults]
-    [compojure.core     :as comp :refer [defroutes GET POST]]
-    [compojure.route    :as route]
-    [hiccup.core        :as hiccup]
-    [clojure.core.async :as async  :refer [<! <!! >! >!! put! chan go go-loop]]
-    [taoensso.timbre    :as timbre :refer [tracef debugf infof warnf errorf]]
-    [taoensso.sente     :as sente]
-    [system.repl :refer [system]]))
+    [com.stuartsierra.component :as component]
+    [compojure.core :as comp :refer [routes GET POST]]
+    [compojure.route :as route]
+    [hiccup.core :as hiccup]
+    [clojure.core.async :as async :refer [<! <!! >! >!! put! chan go go-loop]]
+    [taoensso.timbre :as timbre :refer [tracef debugf infof warnf errorf]]
+    [taoensso.sente :as sente]))
 
 ;; (timbre/set-level! :trace) ; Uncomment for more logging
-(reset! sente/debug-mode?_ true) ; Uncomment for extra debug info
+(reset! sente/debug-mode?_ true)                            ; Uncomment for extra debug info
 
 ;;;; Ring handlers
 
@@ -38,7 +38,7 @@
     ;;
     [:hr]
     [:h2 "Step 3: try login with a user-id"]
-    [:p  "The server can use this id to send events to *you* specifically."]
+    [:p "The server can use this id to send events to *you* specifically."]
     [:p
      [:input#input-login {:type :text :placeholder "User-id"}]
      [:button#btn-login {:type "button"} "Secure login!"]]
@@ -46,7 +46,7 @@
     [:hr]
     [:h2 "Step 4: want to re-randomize Ajax/WebSocket connection type?"]
     [:p "Hit your browser's reload/refresh button"]
-    [:script {:src "js/main.js"}] ; Include our cljs target
+    [:script {:src "js/main.js"}]                           ; Include our cljs target
     ))
 
 (defn login-handler
@@ -59,97 +59,87 @@
     (debugf "Login request: %s" params)
     {:status 200 :session (assoc session :uid user-id)}))
 
-(defroutes ring-routes
-           (GET  "/"      ring-req (landing-pg-handler            ring-req))
-           (GET  "/chsk"  ring-req ((:ring-ajax-get-or-ws-handshake (:sente system)) ring-req))
-           (POST "/chsk"  ring-req ((:ring-ajax-post (:sente system)) ring-req))
-           (POST "/login" ring-req (login-handler                 ring-req))
-           (route/resources "/js" {:root "js"}) ; Static files, notably public/main.js (our cljs target)
-           (route/not-found "<h1>Page not found</h1>"))
-
-(def main-ring-handler
-  "**NB**: Sente requires the Ring `wrap-params` + `wrap-keyword-params`
-  middleware to work. These are included with
-  `ring.middleware.defaults/wrap-defaults` - but you'll need to ensure
-  that they're included yourself if you're not using `wrap-defaults`."
-  (ring.middleware.defaults/wrap-defaults
-    ring-routes ring.middleware.defaults/site-defaults))
+(defn ring-routes
+  [_]
+  (routes
+    (GET "/" ring-req (landing-pg-handler ring-req))
+    (POST "/login" ring-req (login-handler ring-req))
+    (route/resources "/js" {:root "js"})                    ; Static files, notably public/main.js (our cljs target)
+    (route/not-found "<h1>Page not found</h1>")))
 
 ;;;; Some server>user async push examples
 
 (defn test-fast-server>user-pushes
   "Quickly pushes 100 events to all connected users. Note that this'll be
   fast+reliable even over Ajax!"
-  []
-  (doseq [uid (:any @(:connected-uids (:sente system)))]
+  [{:keys [chsk-send! connected-uids]}]
+  (doseq [uid (:any @connected-uids)]
     (doseq [i (range 100)]
-      ((:chsk-send! (:sente system)) uid [:fast-push/is-fast (str "hello " i "!!")]))))
-
-(comment (test-fast-server>user-pushes))
-
-(defonce broadcast-enabled?_ (atom true))
+      (chsk-send! uid [:fast-push/is-fast (str "hello " i "!!")]))))
 
 (defn start-example-broadcaster!
   "As an example of server>user async pushes, setup a loop to broadcast an
   event to all connected users every 10 seconds"
-  []
+  [{{:keys [chsk-send! connected-uids]} :sente :keys [broadcast-enabled?_]}]
   (let [broadcast!
         (fn [i]
-          (let [uids (:any @(:connected-uids (:sente system)))]
+          (let [uids (:any @connected-uids)]
             (debugf "Broadcasting server>user: %s uids" (count uids))
             (doseq [uid uids]
-              ((:chsk-send! (:sente system)) uid
-                                             [:some/broadcast
-                                              {:what-is-this "An async broadcast pushed from server"
-                                               :how-often "Every 10 seconds"
-                                               :to-whom uid
-                                               :i i}]))))]
+              (chsk-send! uid
+                [:some/broadcast
+                 {:what-is-this "An async broadcast pushed from server"
+                  :how-often    "Every 10 seconds"
+                  :to-whom      uid
+                  :i            i}]))))]
 
     (go-loop [i 0]
       (<! (async/timeout 10000))
       (when @broadcast-enabled?_ (broadcast! i))
       (recur (inc i)))))
 
+(defrecord ExampleBroadcaster [sente broadcast-enabled?_]
+  component/Lifecycle
+  (start [component]
+    (start-example-broadcaster! component)
+    component)
+  (stop [component]
+    component))
+
+(defn new-example-broadcaster
+  []
+  (map->ExampleBroadcaster {}))
+
 ;;;; Sente event handlers
 
 (defmulti -event-msg-handler
           "Multimethod to handle Sente `event-msg`s"
-          :id ; Dispatch on event-id
+          #(:id %2)                                              ; Dispatch on event-id
           )
 
 (defn event-msg-handler
   "Wraps `-event-msg-handler` with logging, error catching, etc."
-  [{:as ev-msg :keys [id ?data event]}]
-  (-event-msg-handler ev-msg) ; Handle event-msgs on a single thread
-  ;; (future (-event-msg-handler ev-msg)) ; Handle event-msgs on a thread pool
-  )
+  [component]
+  (fn [ev-msg]
+    (-event-msg-handler component ev-msg)                             ; Handle event-msgs on a single thread
+    ;; (future (-event-msg-handler component ev-msg)) ; Handle event-msgs on a thread pool
+    ))
 
 (defmethod -event-msg-handler
-  :default ; Default/fallback case (no other matching handler)
-  [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
+  :default                                                  ; Default/fallback case (no other matching handler)
+  [component {:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
   (let [session (:session ring-req)
-        uid     (:uid     session)]
+        uid (:uid session)]
     (debugf "Unhandled event: %s" event)
     (when ?reply-fn
       (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
 
 (defmethod -event-msg-handler :example/test-rapid-push
-  [ev-msg] (test-fast-server>user-pushes))
+  [component ev-msg] (test-fast-server>user-pushes component))
 
 (defmethod -event-msg-handler :example/toggle-broadcast
-  [{:as ev-msg :keys [?reply-fn]}]
+  [{:keys [broadcast-enabled?_]} {:as ev-msg :keys [?reply-fn]}]
   (let [loop-enabled? (swap! broadcast-enabled?_ not)]
     (?reply-fn loop-enabled?)))
 
 ;; TODO Add your (defmethod -event-msg-handler <event-id> [ev-msg] <body>)s here...
-
-
-;;;; Init stuff
-
-(defn start! [] (start-example-broadcaster!))
-
-(defn -main "For `lein run`, etc." [] (start!))
-
-(comment
-  (start!)
-  (test-fast-server>user-pushes))
--- src/clj/example/systems.clj
+++ src/clj/example/systems.clj
@@ -1,22 +1,68 @@
 (ns example.systems
-  (:require 
-   [example.server :refer [event-msg-handler main-ring-handler]]
-   [taoensso.sente.server-adapters.http-kit :refer [get-sch-adapter]]
-   ;; or
-   ;; [taoensso.sente.server-adapters.immutant :refer (sente-web-server-adapter)]
-   ;; Optional, for Transit encoding:
-   [taoensso.sente.packers.transit :as sente-transit]
-   [environ.core :refer [env]]
-   [system.core :refer [defsystem]]
-   (system.components 
-    [http-kit :refer [new-web-server]]
-    [sente :refer [new-channel-sockets]])))
-   
+  (:require
+    [example.server :refer [event-msg-handler ring-routes new-example-broadcaster]]
+    [taoensso.sente.server-adapters.http-kit :refer [get-sch-adapter]]
+    ;; or
+    ;; [taoensso.sente.server-adapters.immutant :refer (sente-web-server-adapter)]
+    ;; Optional, for Transit encoding:
+    [taoensso.sente.packers.transit :as sente-transit]
+    [environ.core :refer [env]]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [system.core :refer [defsystem]]
+    (system.components
+      [http-kit :refer [new-web-server]]
+      [endpoint :refer [new-endpoint]]
+      [middleware :refer [new-middleware]]
+      [handler :refer [new-handler]]
+      [sente :refer [new-channel-socket-server sente-routes]])
+    [com.stuartsierra.component :as component]))
+
 (defsystem dev-system
-  [:web (new-web-server (Integer. ^String (env :http-port)) main-ring-handler)
-   :sente (new-channel-sockets event-msg-handler (get-sch-adapter))])
+  [:sente-endpoint (component/using
+                     (new-endpoint sente-routes)
+                     [:sente])
+   :app-endpoints (new-endpoint ring-routes)
+   :example-broadcaster (component/using
+                          (new-example-broadcaster)
+                          [:sente :broadcast-enabled?_])
+   :broadcast-enabled?_ (atom true)
+   ; **NB**: Sente requires the Ring `wrap-params` + `wrap-keyword-params`
+   ; middleware to work. These are included with
+   ; `ring.middleware.defaults/wrap-defaults` - but you'll need to ensure
+   ; that they're included yourself if you're not using `wrap-defaults`.
+   :middleware (new-middleware {:middleware [[wrap-defaults site-defaults]]})
+   :handler (component/using
+              (new-handler)
+              [:sente-endpoint :app-endpoints :middleware])
+   :http (component/using
+           (new-web-server (Integer. ^String (env :http-port)))
+           [:handler])
+   :sente (component/using
+            (new-channel-socket-server event-msg-handler (get-sch-adapter) {:wrap-component? true})
+            [:broadcast-enabled?_])])
 
 (defsystem prod-system
-  [:web (new-web-server (Integer. ^String (env :http-port)) main-ring-handler)
-   :sente (new-channel-sockets event-msg-handler (get-sch-adapter)
-                               {:packer (sente-transit/get-transit-packer :edn)})])
+  [:sente-endpoint (component/using
+                     (new-endpoint sente-routes)
+                     [:sente])
+   :app-endpoints (new-endpoint ring-routes)
+   :example-broadcaster (component/using
+                          (new-example-broadcaster)
+                          [:sente :broadcast-enabled?_])
+   :broadcast-enabled?_ (atom true)
+   ; **NB**: Sente requires the Ring `wrap-params` + `wrap-keyword-params`
+   ; middleware to work. These are included with
+   ; `ring.middleware.defaults/wrap-defaults` - but you'll need to ensure
+   ; that they're included yourself if you're not using `wrap-defaults`.
+   :middleware (new-middleware {:middleware [[wrap-defaults site-defaults]]})
+   :handler (component/using
+              (new-handler)
+              [:sente-endpoint :app-endpoints :middleware])
+   :http (component/using
+           (new-web-server (Integer. ^String (env :http-port)))
+           [:handler])
+   :sente (component/using
+            (new-channel-socket-server event-msg-handler (get-sch-adapter)
+                               {:packer (sente-transit/get-transit-packer :edn)
+                                :wrap-component? true})
+            [:broadcast-enabled?_])])
